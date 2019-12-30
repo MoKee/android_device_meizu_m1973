eb89f672961a55d6b6a99fc1e9a93909994ffeee
XiNGRZ <chenxingyu92@gmail.com>
2019-12-30 19:08:47 +0800

    m1973: Bind mount fstab.qcom to kill FBE
    Change-Id: I4d085390eaa853a80679b6ba70b9361689d1cd0a

--- device.mk
+++ device.mk
@@ -11,5 +11,12 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+# Init
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vendor_fstab.qcom:$(TARGET_COPY_OUT_SYSTEM)/etc/vendor_fstab.qcom
+
 # Inherit from sm8150-common
 $(call inherit-product, device/meizu/sm8150-common/common.mk)
