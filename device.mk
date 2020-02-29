acb90668410399d1508e7da8d2bdaf0e6f1b8c8f
XiNGRZ <chenxingyu92@gmail.com>
2020-02-29 22:21:07 +0800

    m1973: overlay: Move to overlay-mokee
    Change-Id: I4cb2c8f2723d93310362f0b654977ce5f8d95383

--- device.mk
+++ device.mk
@@ -7,9 +7,7 @@
 $(call inherit-product, vendor/meizu/m1973/m1973-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
 
 # Input
 PRODUCT_COPY_FILES += \
